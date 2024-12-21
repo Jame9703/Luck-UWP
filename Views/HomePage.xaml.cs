8dada35be27b490a4faf2782f9bb98de6a3f73a7
Jame9703 <ncsb1213444@outlook.com>
2024-12-21 22:25:14 +0800

    更新

--- Views/HomePage.xaml.cs
+++ Views/HomePage.xaml.cs
@@ -1,6 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Media.Animation;
 
 // https://go.microsoft.com/fwlink/?LinkId=234238 上介绍了“空白页”项模板
 
@@ -11,26 +14,43 @@ namespace Luck
     /// </summary>
     public sealed partial class HomePage : Page
     {
+        private List<string> nameList;
+        private bool isScrolling = false;
+        private Task scrollingTask;
         public HomePage()
         {
             this.InitializeComponent();
-            StartAnimation();
+            nameList = new List<string> { "张三", "李四", "王五", "赵六", "孙七", "周八", "吴九" };
         }
-        private void StartAnimation()
+
+        private async Task ScrollNamesAsync()
         {
-            DoubleAnimation verticalAnimation = new DoubleAnimation();
-            verticalAnimation.From = 0;
-            verticalAnimation.To = 100;
-            verticalAnimation.Duration = TimeSpan.FromSeconds(10);  // 动画持续时间，可调整
-            verticalAnimation.RepeatBehavior = RepeatBehavior.Forever;
-            verticalAnimation.AutoReverse = false;
+            Random random = new Random();
+            while (isScrolling)
+            {
+                var shuffledNames = nameList.OrderBy(n => random.Next()).ToList();
+                NameListBox.ItemsSource = shuffledNames;
+                NameListBox.SelectedIndex = random.Next(shuffledNames.Count);
+                await Task.Delay(10);
+            }
+        }
 
-            Storyboard.SetTarget(verticalAnimation, scrollViewerForAnimation);
-            Storyboard.SetTargetProperty(verticalAnimation, "(ScrollViewer.VerticalOffset)");
-
-            Storyboard storyboard = new Storyboard();
-            storyboard.Children.Add(verticalAnimation);
-            storyboard.Begin(); 
+        private async void StartorStopButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (!isScrolling)//未在滚动，本次点击按钮开始
+            {
+                isScrolling = true;
+                WinnerBorder.Visibility = Visibility.Collapsed;
+                scrollingTask = ScrollNamesAsync();
+                await scrollingTask;
+            }
+            else//已在滚动，本次点击按钮停止
+            {
+                isScrolling = false;
+                var winner = NameListBox.Items[NameListBox.SelectedIndex] as string;
+                WinnerTextBlock.Text = winner;
+                WinnerBorder.Visibility = Visibility.Visible;
+            }
         }
     }
 }
