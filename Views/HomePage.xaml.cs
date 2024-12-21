9d80cb407719f6d05be202babd98800581c76ce3
Jame9703 <ncsb1213444@outlook.com>
2024-12-22 00:36:58 +0800

    #

--- Views/HomePage.xaml.cs
+++ Views/HomePage.xaml.cs
@@ -1,9 +1,12 @@
-﻿using System;
+﻿using Luck.Classes;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Windows.UI;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
 
 // https://go.microsoft.com/fwlink/?LinkId=234238 上介绍了“空白页”项模板
 
@@ -30,6 +33,17 @@ namespace Luck
             {
                 var shuffledNames = nameList.OrderBy(n => random.Next()).ToList();
                 NameListBox.ItemsSource = shuffledNames;
+                //int i = 0;
+                //while (i < shuffledNames.Count)
+                //{
+                //    NameListBox.SelectedIndex = i;
+                //    await Task.Delay(10);
+                //    i++;
+                //    if (i == shuffledNames.Count)
+                //    {
+                //        i = 0;
+                //    }
+                //}
                 NameListBox.SelectedIndex = random.Next(shuffledNames.Count);
                 await Task.Delay(10);
             }
@@ -40,6 +54,10 @@ namespace Luck
             if (!isScrolling)//未在滚动，本次点击按钮开始
             {
                 isScrolling = true;
+                StartorStopButton.Background = new SolidColorBrush(Windows.UI.Colors.Red);
+                StartorStopButtonSymbolIcon.Symbol = Symbol.Stop;
+                StartorStopButtonTextBlock.Text = "停止";
+                //StartorStopButton.RippleHelper.RippleColor = Colors.Red;
                 WinnerBorder.Visibility = Visibility.Collapsed;
                 scrollingTask = ScrollNamesAsync();
                 await scrollingTask;
@@ -47,6 +65,9 @@ namespace Luck
             else//已在滚动，本次点击按钮停止
             {
                 isScrolling = false;
+                StartorStopButton.Background = new SolidColorBrush(Color.FromArgb(255, 192, 242, 188));
+                StartorStopButtonSymbolIcon.Symbol = Symbol.Play;
+                StartorStopButtonTextBlock.Text = "停止";
                 var winner = NameListBox.Items[NameListBox.SelectedIndex] as string;
                 WinnerTextBlock.Text = winner;
                 WinnerBorder.Visibility = Visibility.Visible;
